9f6243678eebad8b359c03d89e4eb4e21e1cc038
Zamira <keldibaevaz@gmail.com>
2026-07-01 13:15:39 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonFour/AidaTask.java
@@ -0,0 +1,48 @@
+package com.company.lessonFourSwitch.lessonFour;
+
+import java.util.Scanner;
+
+public class AidaTask {
+
+    public static void main(String[] args) {
+
+        /**
+         * 0 - 24
+         * работаем - 9 - 17
+         * обед - 13-14
+         * не работаем - ????
+         *
+         * 25  жок
+         * -1  жок
+         *
+         * if - else if
+         *
+         * */
+        System.out.println("Введите время: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if ((a >= 9 && a < 13) || (a >= 15 && a <= 17)) {
+            System.out.println("Вы работаете");
+        } else if (a == 13 || a == 14) {
+            System.out.println("У вас перерыв");
+        } else if ((a >= 18 && a <= 24) || (a < 9 && a >= 0)) {
+            System.out.println("Вы не работаете");
+        } else {
+            System.out.println("Вы ввели неверное число");
+        }
+    }
+
+/*    if (a >= 9 && a < 13) {
+        System.out.println("Вы работаете");
+    } else if (a == 13 || a == 14) {
+        System.out.println("У вас перерыв");
+    } else if (a >= 15 && a <= 17) {
+        System.out.println("Вы так же работаете");
+    } else if (a >= 18 && a <= 24) {
+        System.out.println("Вы не работаете");
+    } else if (a < 9 && a >= 0) {
+        System.out.println("Вы не работаете");
+    } else {
+        System.out.println("Вы ввели неверное число");
+    }*/
+}
