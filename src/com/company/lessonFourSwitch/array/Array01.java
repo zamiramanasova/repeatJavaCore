b9863681a0b2be342aec4419457d7ec6ff915521
Zamira <keldibaevaz@gmail.com>
2026-07-01 13:46:39 +0300

    first

---
+++ src/com/company/lessonFourSwitch/array/Array01.java
@@ -0,0 +1,20 @@
+package com.company.lessonFourSwitch.array;
+
+public class Array01 {
+    public static void main(String[] args) {
+
+        int[] array = new int[5];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+}
