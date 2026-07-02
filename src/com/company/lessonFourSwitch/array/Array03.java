8ccdd1c900fadefefaa784fb6ed16826cb4e9feb
Zamira <keldibaevaz@gmail.com>
2026-07-02 14:20:45 +0300

    first

---
+++ src/com/company/lessonFourSwitch/array/Array03.java
@@ -0,0 +1,18 @@
+package com.company.lessonFourSwitch.array;
+
+public class Array03 {
+    public static void main(String[] args) {
+
+//        int[] array = new int[]{1,2,3,4,5};
+//
+//        for(int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
+
+        int[] arr = new int[5];
+
+        for(int o = 0; o < arr.length; o++){
+            System.out.println(arr[o]);
+        }
+    }
+}
