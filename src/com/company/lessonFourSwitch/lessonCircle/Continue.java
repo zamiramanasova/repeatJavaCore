fa098a4bd3e8eb2aa3ceba29b777672e8edad4e7
Zamira <keldibaevaz@gmail.com>
2026-07-03 12:52:29 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonCircle/Continue.java
@@ -0,0 +1,18 @@
+package com.company.lessonFourSwitch.lessonCircle;
+
+public class Continue {
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (counter < 3) {
+            counter++;
+            System.out.println("Counter: " + counter);
+
+            if (true) continue;// с пропуском итерации continue
+
+            System.out.println("Это строка не выполнится");
+        }
+        System.out.println("Произведено " + counter + " итераций.");
+    }
+}
