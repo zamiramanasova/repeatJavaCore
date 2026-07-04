9e9eb7f6411254481c328fa387c421cb4c786496
Zamira <keldibaevaz@gmail.com>
2026-07-04 19:51:26 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonCircle/DijcstralLoop.java
@@ -0,0 +1,25 @@
+package com.company.lessonFourSwitch.lessonCircle;
+
+import java.util.Scanner;
+
+public class DijcstralLoop {
+    public static void main(String[] args) {
+        // БЕСКОНЕЧНЫЙ ЦИКЛ С ПРЕРЫВАНИЕ ОПЕРАТОРА - BREAK
+        int character;
+        Scanner in = new Scanner(System.in);
+
+        for  (; ; ) {
+            character = in.next().charAt(0);
+
+            switch (character) {
+                case 'l':
+                    System.out.println("Go left");
+                    continue;
+                case 'r':
+                    System.out.println("Go right");
+                    continue;
+            }
+            break;// прерывание цикла
+        }
+    }
+}
