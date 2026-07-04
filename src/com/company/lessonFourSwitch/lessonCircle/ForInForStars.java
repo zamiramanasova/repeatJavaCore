52411b8a3d627ae4d01ca388b0e002142a3d69c0
Zamira <keldibaevaz@gmail.com>
2026-07-04 19:53:39 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonCircle/ForInForStars.java
@@ -0,0 +1,22 @@
+package com.company.lessonFourSwitch.lessonCircle;
+
+public class ForInForStars {
+    public static void main(String[] args) {
+
+//        for(int counter = 0; counter < 10; counter++) {
+//
+//            for (int i = 0; i < 10; i++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        for(int counter = 0; counter < 10; counter++) {
+
+            for (int i = 0; i < counter; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
