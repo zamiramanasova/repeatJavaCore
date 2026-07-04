978c36f21ee28bb9b8eb7f752fa3ac03afcb8800
Zamira <keldibaevaz@gmail.com>
2026-07-04 19:51:52 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonCircle/DoWhile.java
@@ -0,0 +1,15 @@
+package com.company.lessonFourSwitch.lessonCircle;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+        int counter = 0;
+
+        do {
+            counter++;
+            System.out.println("Counter: "+ counter);
+        } while (counter < 3);
+
+        System.out.println("Произведено " + counter + " итераций");
+    }
+}
