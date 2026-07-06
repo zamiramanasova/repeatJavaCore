71854ad55ae2ba32887cad8fee63cabcdfeb9fc3
Zamira <keldibaevaz@gmail.com>
2026-07-06 15:11:10 +0300

    first

---
+++ src/com/company/lessonFourSwitch/leetCode/LeetCode.java
@@ -0,0 +1,20 @@
+package com.company.lessonFourSwitch.leetCode;
+
+public class LeetCode {
+    public static void main(String[] args) {
+        mySqrt(4);
+        System.out.println(mySqrt(4));
+    }
+
+    public static int mySqrt(int x) {
+        int num = 1;
+        int rez = 0;
+        while(x>0){
+            x-=num;
+            num+=2;
+            rez+=(x<0)?0:1;
+        }
+        return rez;
+    }
+
+}
