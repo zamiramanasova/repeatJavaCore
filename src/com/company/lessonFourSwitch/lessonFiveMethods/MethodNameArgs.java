1d93db40ff99dd78fc877a1aaed0cc8cd5c69176
Zamira <keldibaevaz@gmail.com>
2026-07-07 17:48:31 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonFiveMethods/MethodNameArgs.java
@@ -0,0 +1,16 @@
+package com.company.lessonFourSwitch.lessonFiveMethods;
+
+public class MethodNameArgs {
+
+    static int difference(int height, int width) {
+        return height - width;
+    }
+
+    public static void main(String[] args) {
+        //можно вызвать метод так
+        int difference;
+        difference = difference(6,5);
+
+        System.out.println("Разность равна: "+ difference);
+    }
+}
