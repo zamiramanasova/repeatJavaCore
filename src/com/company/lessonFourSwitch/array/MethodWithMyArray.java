3794f0c5d910b4f6f25ce2416adf564fde5fd865
Zamira <keldibaevaz@gmail.com>
2026-07-08 23:32:36 +0300

    first

---
+++ src/com/company/lessonFourSwitch/array/MethodWithMyArray.java
@@ -0,0 +1,21 @@
+package com.company.lessonFourSwitch.array;
+
+public class MethodWithMyArray {
+
+    static int[] modifyArray(int[] array, int modifier) {
+        for (int i =0; i < array.length; i++) {
+            array[i] = array[i] * modifier;
+        }
+        return  array;
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {1,2,3,4,5};
+
+        myArray = modifyArray(myArray, 5);
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i] + ", ");
+        }
+    }
+}
