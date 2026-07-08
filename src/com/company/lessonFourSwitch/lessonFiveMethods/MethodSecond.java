c8061c480510f27ec74c3516bf220c8a03d99643
Zamira <keldibaevaz@gmail.com>
2026-07-08 23:31:27 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonFiveMethods/MethodSecond.java
@@ -0,0 +1,19 @@
+package com.company.lessonFourSwitch.lessonFiveMethods;
+
+public class MethodSecond {
+
+    static String compare(int value1, int value2) {
+        if (value1 < value2) {
+            return "Comparison Less Than";
+        }else if (value1 > value2) {
+            return "Comparison Greater Than";
+        }
+        return "Comparison Equal";
+    }
+
+    public static void main(String[] args) {
+        int operand1 = 1, operand2 = 2;
+        String result = compare(operand1,operand2);
+        System.out.println(result);
+    }
+}
