41ec2c8c6a31686f59e5fca3ca146d06d900e539
Zamira <keldibaevaz@gmail.com>
2026-07-08 23:34:43 +0300

    first

---
+++ src/com/company/lessonFourSwitch/leetCode/PalindromeString.java
@@ -0,0 +1,26 @@
+package com.company.lessonFourSwitch.leetCode;
+
+/***
+ * Программа по выводу палиндрома с помощью String.
+ */
+public class PalindromeString {
+    public static void main(String[] args) {
+        String s = "ABCBA";
+
+        System.out.println(isPalindrome(s) ? "YES" : "NO");
+    }
+
+    private static boolean isPalindrome(String s) {
+        //base step
+        if(s.length() == 1 || s.length() == 0)
+            return true; //тогда палиндром
+
+        //если s чарет ноль равняется  s.charAt(s.length() - 1) с последним, то
+        // то тогда мы продолжаем вызывать палиндром
+        if(s.charAt(0) == s.charAt(s.length() - 1))
+            //при возвр true, то я беру стринг и обрезаю с обеих сторон похожие
+            return isPalindrome(s.substring(1, s.length() - 1));// шаг рекурсия
+        // это относится ко второму if else
+        return false;
+    }
+}
