90bca2e9f24e74638d1ec447844eb02d4ff7b8d5
Zamira <keldibaevaz@gmail.com>
2026-07-09 15:28:06 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonFiveMethods/ReturnMethodInt.java
@@ -0,0 +1,28 @@
+package com.company.lessonFourSwitch.lessonFiveMethods;
+
+import java.util.Scanner;
+
+public class ReturnMethodInt {
+
+    static int addTwo(int a) {
+        a = a + 2;
+        System.out.println("a = " + a);
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите число: ");
+        Scanner in =new Scanner(System.in);
+
+        String number = in.next();
+        int result = Integer.parseInt(number);
+
+        System.out.println("Значение result = " + result);
+        result = 10;
+        //в качестве аргумента передается не сама переменная = result, а ее копия
+        result = addTwo(result);
+        System.out.println(addTwo(result));
+        System.out.println(result);
+    }
+}
