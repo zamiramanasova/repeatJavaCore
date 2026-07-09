2161754123e2144762579d41865ebff5e8118bb9
Zamira <keldibaevaz@gmail.com>
2026-07-09 15:26:48 +0300

    first

---
+++ src/com/company/lessonFourSwitch/practice/Practice180922.java
@@ -0,0 +1,23 @@
+package com.company.lessonFourSwitch.practice;
+
+import com.company.lessonFourSwitch.lessonFour.Scannerz;
+
+import java.util.Scanner;
+
+/**
+ * 31. Напишите программу, которая принимает три числа от пользователя и
+ * печатает «увеличивающиеся», если числа расположены в порядке возрастания,
+ * «убывающие», если числа находятся в порядке убывания, и «Ни возрастающий,
+ * ни убывающий порядок» в противном случае.
+ */
+public class Practice180922 {
+    public static void main(String[] args) {
+//        System.out.println(" Введите число: ");
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        if (a < b || a < c ||  )
+
+    }
+}
