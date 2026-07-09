11e354428ddb4fef403f9520479fa164f134c489
Zamira <keldibaevaz@gmail.com>
2026-07-09 15:28:38 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonFour/Scannerz.java
@@ -0,0 +1,14 @@
+package com.company.lessonFourSwitch.lessonFour;
+
+import java.util.Scanner;
+
+public class Scannerz {
+    public static void main(String[] args) {
+        System.out.println("Just input Scanner");
+
+        Scanner input = new Scanner(System.in);
+        String buffer = input.nextLine();
+
+        System.out.println(buffer);
+    }
+}
