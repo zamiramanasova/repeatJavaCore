4203b526d4a56b55fe504ce60c588d6a7e276759
Zamira <keldibaevaz@gmail.com>
2026-07-13 18:50:45 +0300

    first

---
+++ src/com/company/lessonFourSwitch/array/TwoDimensionalArray.java
@@ -0,0 +1,32 @@
+package com.company.lessonFourSwitch.array;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        //три на три создастся три на три массив, как матрица
+        int[][] array = new int[3][3];
+
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+
+        array[1][0] = 4;
+        array[1][1] = 5;
+        array[1][2] = 6;
+
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
+
+        System.out.println(array[0][0]);
+        System.out.println(array[0][1]);
+        System.out.println(array[0][2]);
+        System.out.print('\n');
+        System.out.println(array[1][0]);
+        System.out.println(array[1][1]);
+        System.out.println(array[1][2]);
+        System.out.print('\n');
+        System.out.println(array[2][0]);
+        System.out.println(array[2][1]);
+        System.out.println(array[2][2]);
+    }
+}
