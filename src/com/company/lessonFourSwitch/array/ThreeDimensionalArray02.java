8238834530da50efad540a03ab3e98df3b2de2b5
Zamira <keldibaevaz@gmail.com>
2026-07-13 18:49:35 +0300

    first

---
+++ src/com/company/lessonFourSwitch/array/ThreeDimensionalArray02.java
@@ -0,0 +1,33 @@
+package com.company.lessonFourSwitch.array;
+
+public class ThreeDimensionalArray02 {
+    public static void main(String[] args) {
+        int[][][] array = {
+                {
+                        {1,2,3},
+                        {4,5,6},
+                        {7,8,9}
+                },
+                {
+                        {1,2,3},
+                        {4,5,6},
+                        {7,8,9}
+                },
+                {
+                        {1,2,3},
+                        {4,5,6},
+                        {7,8,9}
+                }
+        };
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    System.out.print(array[i][j][k] + " ");
+                }
+                System.out.print("\n");
+            }
+            System.out.print("\n");
+        }
+    }
+}
