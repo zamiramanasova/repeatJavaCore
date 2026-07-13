d4095affc810df7eb0ea8c992985e991e89bcf72
Zamira <keldibaevaz@gmail.com>
2026-07-13 18:50:10 +0300

    first

---
+++ src/com/company/lessonFourSwitch/leetCode/ToLowerCase.java
@@ -0,0 +1,16 @@
+package com.company.lessonFourSwitch.leetCode;
+
+/**
+ * 709. toLowerCase
+ */
+public class ToLowerCase {
+    public static void main(String[] args) {
+
+        String s = "Hello";
+        System.out.println(toLowerCase(s.toLowerCase()));
+    }
+    public static String toLowerCase(String s) {
+        s.toLowerCase();
+        return s;
+    }
+}
