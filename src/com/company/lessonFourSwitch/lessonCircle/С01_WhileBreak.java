a10bf6e5b66e38931c58ba2709454d35bc3c8380
Zamira <keldibaevaz@gmail.com>
2026-07-14 15:39:39 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonCircle/С01_WhileBreak.java
@@ -0,0 +1,16 @@
+package com.company.lessonFourSwitch.lessonCircle;
+
+public class С01_WhileBreak {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while (counter < 3) {
+            counter++;
+            System.out.println("Counter " + counter);
+
+            if (true) break;// если правда то мы выходим из цикла
+            System.out.println("Это строка не выполнится");
+        }
+        System.out.println("Произведено " + counter + " итераций.");
+    }
+}
