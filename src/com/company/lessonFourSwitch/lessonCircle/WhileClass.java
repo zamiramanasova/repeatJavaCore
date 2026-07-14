592c0f70387d0bb360eb8ecf10c5b3b37837d27c
Zamira <keldibaevaz@gmail.com>
2026-07-14 15:39:11 +0300

    first

---
+++ src/com/company/lessonFourSwitch/lessonCircle/WhileClass.java
@@ -0,0 +1,13 @@
+package com.company.lessonFourSwitch.lessonCircle;
+
+public class WhileClass {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while (counter < 3) {
+            counter++;
+            System.out.println("Counter: " + counter);
+        }
+        System.out.println("Произведено " + counter + " итераций.");
+    }
+}
