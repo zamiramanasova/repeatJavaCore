773518181688b9754cf8947349a032eba366a5c5
Zamira <keldibaevaz@gmail.com>
2026-07-14 15:38:18 +0300

    first

---
+++ src/com/company/lessonFourSwitch/array/TwoDimensionalArrayOnlyOne.java
@@ -0,0 +1,14 @@
+package com.company.lessonFourSwitch.array;
+
+public class TwoDimensionalArrayOnlyOne {
+    public static void main(String[] args) {
+        int[] vector = new int[1];
+        vector[0] =100;
+        System.out.println(vector[0]);
+
+        int[][] matrix = new int[1][1];
+        matrix[0][0] = 200;
+        System.out.println(matrix[0][0]);
+
+    }
+}
