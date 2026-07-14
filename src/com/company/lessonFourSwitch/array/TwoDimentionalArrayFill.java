2d95b075fc855743a09a15bcb9fc00eca4e27dd0
Zamira <keldibaevaz@gmail.com>
2026-07-14 15:38:44 +0300

    first

---
+++ src/com/company/lessonFourSwitch/array/TwoDimentionalArrayFill.java
@@ -0,0 +1,23 @@
+package com.company.lessonFourSwitch.array;
+
+public class TwoDimentionalArrayFill {
+    public static void main(String[] args) {
+        int sizeI = 3;
+        int sizeJ = 3;
+        int[][] array = new int[sizeI][sizeJ];
+
+        //заполнение массива
+        for(int i =0; i < sizeI; i++) {
+            for(int j = 0; j < sizeJ; j++) {
+                array[i][j] = i * j + 1;
+            }
+        }
+        //вывод массива
+        for (int i = 0; i < sizeJ; i++) {
+            for(int j = 0; j < sizeJ; j++) {
+                System.out.println(" " + array[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+}
