05fde3594cd89fb8218d0463c74aa63ed544c3d3
Zamira <keldibaevaz@gmail.com>
2026-07-16 20:49:41 +0300

    first

--- src/com/company/lessonFourSwitch/leetCode/PalindromeString.java
+++ src/com/company/lessonFourSwitch/leetCode/PalindromeString.java
@@ -18,7 +18,7 @@ public class PalindromeString {
         // если s чарет ноль равняется  s.charAt(s.length() - 1) с последним, то
         // то тогда мы продолжаем вызывать палиндром
         if(s.charAt(0) == s.charAt(s.length() - 1))
-            //при возвр true, то я беру стринг и обрезаю с обеих сторон похожие
+            // при возвр true, то я беру стринг и обрезаю с обеих сторон похожие
             return isPalindrome(s.substring(1, s.length() - 1));// шаг рекурсия
         // это относится ко второму if else
         return false;
