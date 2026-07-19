6cd7627281096e52249fe9f2cc7c8c4705282f4f
Zamira <keldibaevaz@gmail.com>
2026-07-19 13:32:24 +0300

    first

--- src/com/company/lessonFourSwitch/lessonFiveLogicOperation/C05_Logic.java
+++ src/com/company/lessonFourSwitch/lessonFiveLogicOperation/C05_Logic.java
@@ -3,7 +3,7 @@ package com.company.lessonFourSwitch.lessonFiveLogicOperation;
 public class C05_Logic {
     public static void main(String[] args) {
 
-        //Логические операции
+        // Логические операции
         //Чтобы проверить условие А < x < B, нельзя записать его в условном операторе непосредственно,
         //так как каждая операция отношения должна иметь два операнда
         //правильный способ записи: if (A < x && x < B)
