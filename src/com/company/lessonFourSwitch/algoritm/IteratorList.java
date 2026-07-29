537a9c2453ca25b1487e154f67f4ccfedff77cfa
Zamira <keldibaevaz@gmail.com>
2026-07-29 12:18:47 +0300

    first

--- src/com/company/lessonFourSwitch/algoritm/IteratorList.java
+++ src/com/company/lessonFourSwitch/algoritm/IteratorList.java
@@ -10,6 +10,7 @@ public class IteratorList {
         /**
          * функция удаления с итератора с помощью метода remove.
          */
+
         List<Integer> list = new ArrayList<>();
         list.add(0,12);
         list.add(1,13);
