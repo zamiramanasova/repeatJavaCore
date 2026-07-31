e9f57c2c6b2bbc779299f27c43c31b85c6317df0
Zamira <keldibaevaz@gmail.com>
2026-07-31 16:40:14 +0300

    first

--- src/com/company/lessonFourSwitch/lessonCircle/EndLessLoop.java
+++ src/com/company/lessonFourSwitch/lessonCircle/EndLessLoop.java
@@ -8,7 +8,7 @@ public class EndLessLoop {
             System.out.println("Hello");
         }
 
-//        do {
+//       do {
 //            System.out.println("Hello");
 //        }
 //        while (true);
