fa642fa37ecd0f16399cb75f996044d9bd23790c
Zamira <keldibaevaz@gmail.com>
2026-08-03 20:00:06 +0300

    first

--- src/com/company/lessonFourSwitch/lessonCircle/HomeWorkAida.java
+++ src/com/company/lessonFourSwitch/lessonCircle/HomeWorkAida.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class HomeWorkAida {
     public static void main(String[] args) {
+
         // 1 FOR
         /**
          числа от 1 до 100, которые делятся на 3 и на 5 без остатка &&  %
