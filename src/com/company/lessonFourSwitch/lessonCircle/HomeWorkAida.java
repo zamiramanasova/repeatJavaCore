33c60bc6ec8ef4334065be4fb09d56fb4fb11887
Zamira <keldibaevaz@gmail.com>
2026-08-05 20:12:32 +0300

    first

--- src/com/company/lessonFourSwitch/lessonCircle/HomeWorkAida.java
+++ src/com/company/lessonFourSwitch/lessonCircle/HomeWorkAida.java
@@ -35,7 +35,7 @@ public class HomeWorkAida {
  * на 13 без остатка.
  * Ответ = ???
  * */
-//       int count = 0;
+//      int count = 0;
 //        for (int h = 1; h < 1000; h++) {
 //            if (h % 13 == 0) {
 //                count++;
